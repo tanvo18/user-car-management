05a14e70d150f0a2515a98442f386c091dec65c7
Tan Vo <tanvo.bks2014@gmail.com>
2023-01-02 15:45:19 +0800

    Update Swagger

--- backend/src/car/dto/availability.dto.ts
+++ backend/src/car/dto/availability.dto.ts
@@ -1,17 +1,32 @@
 import { ApiProperty } from '@nestjs/swagger';
 import { Type } from 'class-transformer';
-import { IsDate, IsEnum, IsNotEmpty, IsNumber, ValidateNested } from 'class-validator';
+import {
+  IsDate,
+  IsEnum,
+  IsNotEmpty,
+  IsNumber,
+  ValidateNested,
+} from 'class-validator';
 import { CarStatus } from '../../availability/entity/availability.entity';
 export class AvailabilityDto {
+  @ApiProperty({
+    required: true,
+  })
   @IsNotEmpty()
   @Type(() => Date)
   @IsDate()
   date: Date;
 
+  @ApiProperty({
+    required: true,
+  })
   @IsNumber()
   @IsNotEmpty()
   price: number;
 
+  @ApiProperty({
+    required: true,
+  })
   @IsNotEmpty()
   @IsEnum(CarStatus)
   status: CarStatus;
@@ -19,14 +34,14 @@ export class AvailabilityDto {
 
 export class setAvailabilityDto {
   @ApiProperty({
-    required: false,
+    required: true,
   })
   @IsNotEmpty()
   @IsNumber()
   carId: number;
 
   @ApiProperty({
-    required: false,
+    required: true,
   })
   @ValidateNested({ each: true })
   @Type(() => AvailabilityDto)
--- backend/src/car/dto/car-info.dto.ts
+++ backend/src/car/dto/car-info.dto.ts
@@ -3,7 +3,7 @@ import { IsNotEmpty } from 'class-validator';
 
 export class CarInfoDto {
   @ApiProperty({
-    required: false,
+    required: true,
   })
   @IsNotEmpty()
   model: string;
